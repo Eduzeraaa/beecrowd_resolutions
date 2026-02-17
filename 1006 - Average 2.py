8efb74526f04750a9c2518b55889e707e39ae1ab
Eduardo <eduardoluisbsb2@gmail.com>
2026-02-17 20:56:51 -0300

    Add program to calculate weighted average

---
+++ 1006 - Average 2.py
@@ -0,0 +1,12 @@
+A = float(input(""))
+B = float(input(""))
+C = float(input(""))
+
+weightA = 2
+weightB = 3
+weightC = 5
+
+
+MEDIA = (A * weightA + B * weightB + C * weightC) / (weightA + weightB + weightC)
+
+print(f'MEDIA = {MEDIA:.1f}')
