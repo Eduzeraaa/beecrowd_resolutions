1315ba0e2d0497b92e6ba3a420dc64de0a4a4f44
Eduardo <eduardoluisbsb2@gmail.com>
2026-02-17 21:18:07 -0300

    Create  1080 - Highest and Position.py

---
+++ 1080 - Highest and Position.py
@@ -0,0 +1,9 @@
+numeros = []
+for _ in range(100):
+    numero = int(input())
+    numeros.append(numero)
+    maior = max(numeros)
+    posicao = numeros.index(maior) + 1
+
+print(maior)
+print(posicao)
