2ac18b8caad26f05ef7ccf1926b0bbc9c0e7a1b0
Eduardo <eduardoluisbsb2@gmail.com>
2026-02-17 21:00:07 -0300

    Add area calculations for triangle, circle, trapezoid, square, and rectangle
    Calculate areas of various geometric shapes using inputs.

---
+++ 1012 - Area.py
@@ -0,0 +1,11 @@
+A, B, C = input().split()
+A = float(A)
+B = float(B)
+C = float(C)
+pi = 3.14159
+
+triangulo = (A * C) / 2
+circulo = pi * C**2
+trapezio = ((A + B) / 2) * C
+quadrado = B**2
+retangulo = A * B
